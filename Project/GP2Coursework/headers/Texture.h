06c31fc7f54df69522316daf540c2d0dc7b6b908
euanmcmen <euanmcmen@hotmail.com>
2014-11-18 20:33:38 +0000

    Things work!
    Things include folder re-structuring, dll linking, Apple code purging.

---
+++ Project/GP2Coursework/headers/Texture.h
@@ -0,0 +1,18 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <string>
+
+
+GLuint convertSDLSurfaceToGLTexture(SDL_Surface * surface);
+
+GLuint loadTextureFromFile(const std::string& filename);
+//this shouldn't be used for realtime use, build on startup!
+GLuint loadTextureFromFont(const std::string& fontFilename, int pointSize, const std::string& text);
+
+#endif
